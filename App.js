3808eb718e8f7a4c7b40ca3c153cdcca7bf77cde
Henry <henry.a.s.arrovin@gmail.com>
2023-09-02 11:01:48 +0530

    Initial commit.

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, FlatList, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddTodo';
+import AppBar from './components/AppBar';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'Learn flutter', key: '1'},
+    {text: 'Learn react-native', key: '2'},
+    {text: 'Learn react', key: '3'}
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
+
+  const submitHandler = (text) => {
+    if (text.length != 0){
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString()},
+          ...prevTodos
+        ]
+      });
+    } else {
+      Alert.alert('OOPS!', 'Todos must be over 3 chars long', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
+      ])
+    }
+    
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <AppBar/>
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => 
+                <TodoItem item={item} pressHandler={pressHandler}/>
+              }
+            />
+          </View>
+        </View>
+      </View>
   );
 }
 
@@ -14,7 +55,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
---
+++ components/AppBar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const AppBar = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>Todos</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: "black",
+  },
+  title: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+
+export default AppBar;
